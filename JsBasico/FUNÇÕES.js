20f4f1fa8638de1dec75db4514890fe94f813f74
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-04 12:32:23 -0300

    subir

---
+++ JsBasico/FUNÇÕES.js
@@ -0,0 +1,9 @@
+let valor = 20
+
+function incrementa(){
+    console.log('Estou dentro da função')
+}
+
+console.log(valor)
+
+incrementa()
---
+++ JsBasico/Game01.js
@@ -0,0 +1,16 @@
+function jogo(perso1, perso2){
+    //let perso1 = 35
+    //let perso2 = 35
+    
+    let contra = perso1 -= perso2
+
+    return contra
+}
+
+let perso1 = 35
+let perso2 = 35
+
+
+
+jogo()
+
