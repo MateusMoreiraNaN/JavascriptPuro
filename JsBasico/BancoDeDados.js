6440c598ead248a6625d98b9f4bff6b1a420e479
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-06 00:01:27 -0300

    escopo

--- JsBasico/BancoDeDados.js
+++ JsBasico/BancoDeDados.js
@@ -1,3 +1,19 @@
 const BancoDeDados = () => {
-    
+    let emailsUsuarios = [
+        'beverly.kihn44@gmail.com',
+        'mark_corkery@yahoo.com',
+        'lon_veum4@gmail.com',
+        'lucio56@yahoo.com',
+        'wilson_altenwerth@gmail.com',
+        'samantha23@hotmail.com',
+        'cicero68@yahoo.com',
+        'lucy39@hotmail.com',
+        'preston_hyatt44@yahoo.com',
+        'rossie91@gmail.com',
+        'lane7@gmail.com'
+    ];
+
+    return emailsUsuarios
 }
+
+console.log(BancoDeDados());
---
+++ JsBasico/escopoDeFunção.js
@@ -0,0 +1,26 @@
+function myFucn(){
+    let cat = 'Zeca'
+    const age = 3
+    let color = 'cinza'
+}
+
+function myFucn2(){
+    let cat = 'Zika'
+    const age = 5
+    let color = 'branco'
+}
+
+myFucn()
+console.log(color);
+
+const dog = 'Pastor-alemão'
+
+const dogWatch = () => {
+    const dog = 'Samoieda'
+
+    console.log(dog);
+}
+
+dogWatch()
+
+console.log(dog);
