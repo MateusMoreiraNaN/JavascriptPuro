f8c5132f409ad131f0c652a00f237f1c1c1ad553
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-09 18:52:40 -0300

    listFor

---
+++ JsBasico/EmailArray.js
@@ -0,0 +1,17 @@
+let EmailDb = [
+
+]
+
+// entrada do usuario
+EmailDb.push('kaycee.hoppe39@yahoo.com')
+
+EmailDb.push('ryleigh.heathcote@hotmail.com')
+
+EmailDb.push('jovanny85@hotmail.com')
+
+EmailDb.push('adaline.tremblay@hotmail.com')
+
+for(let email of EmailDb){
+    console.log(email);
+}
+
---
+++ JsBasico/Type.js
@@ -0,0 +1,18 @@
+let nba = [
+        'Celtics',
+        'Nets',
+        'Bucks',
+        'Cavaliers',
+        '76ers',
+        'Pacers',
+        'Knicks',
+        'Heat'
+]
+
+console.log('Conferência Leste');
+console.log(' ');
+
+
+for(let Nba of nba){
+    console.log(Nba);
+}
