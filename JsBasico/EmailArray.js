ce7066bf4172d2bb329c48c1ba2ca3976a879537
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-10 10:28:02 -0300

    baseDedados

--- JsBasico/EmailArray.js
+++ JsBasico/EmailArray.js
@@ -1,3 +1,4 @@
+/*
 let EmailDb = [
 
 ]
@@ -14,4 +15,31 @@ EmailDb.push('adaline.tremblay@hotmail.com')
 for(let email of EmailDb){
     console.log(email);
 }
+*/
+
+function base(baseDeDados){
+    
+    for(let email of baseDeDados){
+        console.log(email);
+    }
+}
+
+let baseDeDados = [
+
+]
+
+
+
+// entrada do usuario
+baseDeDados.push('kaycee.hoppe39@yahoo.com')
+
+baseDeDados.push('ryleigh.heathcote@hotmail.com')
+
+baseDeDados.push('jovanny85@hotmail.com')
+
+baseDeDados.push('adaline.tremblay@hotmail.com')
+
+baseDeDados.push('eldridge_connelly96@gmail.com')
+
+let saida = base(baseDeDados)
 
