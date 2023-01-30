277178d66020f09c68ce004f54d856fe2552f5c0
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-30 12:12:04 -0300

    rpg

--- OO/RPG.js
+++ OO/RPG.js
@@ -18,3 +18,24 @@ class mago extends build{
     forca = 48
 
 }
+
+class guerreiro extends build{
+    forca = 100
+    agilidade = 80
+    resistencia = 78
+}
+
+class ladino extends build{
+    furtividade = 100
+    QI = 95
+    resiliencia = 90
+    forca = 61
+}
+
+class ninja extends ladino{
+    agilidade = 94
+}
+
+let classe1 = new mago(100, 'Java')
+
+console.log(`nick:${classe1.name}\nvida: ${classe1.life} \nQI:${classe1.QI}\nagilidade:${classe1.agilidade}\nMagia:${classe1.magia}\nforça:${classe1.forca}`);
