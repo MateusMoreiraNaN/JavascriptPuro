096b87e83de904e262a319c34be10fbcb39319d1
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-04 11:36:40 -0300

    async

--- Requisição/FetchAPI/JS/script.js
+++ Requisição/FetchAPI/JS/script.js
@@ -2,7 +2,15 @@ function fetchApiData(){
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then((res)=>res.json())
     .then((data) => {
-        
+        const list = document.querySelector('#fill_list')
+
+        data.map((item)=>{
+            const div = document.createElement('div')
+
+            div.setAttribute('id', item.id)
+            div.innerHTML = item.title
+            list.appendChild(div)
+        })
     })
 }
 
