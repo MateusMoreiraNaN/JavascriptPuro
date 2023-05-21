8ad2a02833b56ddd79d93214b986f6ff9191b94c
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-20 23:16:41 -0300

    class

--- cpfAwait.js
+++ cpfAwait.js
@@ -2,17 +2,20 @@ const cpf =()=>{
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
             let cpf = '147.547.654-7'
-            let valite = true
-            if(cpf == valite){
+            let corret = '147.547.654-7' 
+            
+            if(cpf === corret){
                 console.log('CPF VAlIDO');
             }
-        })
+        },2000)
     })
 }
 
 const validador = async ()=>{
     try{
-        
+        await cpf()
+    }catch(erro){
+        console.log('CPF INVALIDO')
     }
 }
 
